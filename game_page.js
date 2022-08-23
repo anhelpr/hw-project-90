edc8ba8573278dc156ed58ff45ab8e07f5267c4e
Angel prajapati <89209140+anhelpr@users.noreply.github.com>
2022-08-23 18:48:58 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,17 @@
+function addUser()
+{
+  player1_name = document.getElementById("player1_name").value;
+  player2_name = document.getElementById("player2_name").value;
+
+  localStorage.setItem("player1_name",player1_name);
+  localStorage.setItem("player2_name",player2_name);
+
+  window.location.replace("game_pages.html")
+}
+
+function send()
+{
+  number1 document.getElementById("number1").value;
+  number2 document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
+}
